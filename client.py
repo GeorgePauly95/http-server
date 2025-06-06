45e8cc74eb79709783af9dabdc1978c5b5753672
georgepauly <georgepauly1995@gmail.com>
2025-06-06 09:13:52 +0530

    Second attempt at echo server

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket
 
-# kill PID 1234 and 1235
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((socket.gethostname(), 1236))
-message = input("Please type your message here, and we'll mirror it back to you:\n\n")
-
-s.send(bytes(message, "utf-8"))
-mirror_message = s.recv(1024)
-print(mirror_message.decode("utf-8"))
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 1237))
+    message = input(
+        "Please type your message here, and we'll mirror it back to you:\n\n"
+    )
+    s.send(bytes(message, "utf-8"))
+    mirror_message = s.recv(10240)
+    output = mirror_message.decode("utf-8")
+    if output != "quit()":
+        s.close()
+        i += 1
+        continue
+    s.close()
+    break
