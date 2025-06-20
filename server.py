d9f4eac4307eeb7436d1850e447fadd34b61602e
georgepauly <georgepauly1995@gmail.com>
2025-06-20 11:38:33 +0530

    added json parser to parse requests with body

--- server.py
+++ server.py
@@ -6,36 +6,34 @@ hs.bind((socket.gethostname(), 2002))
 hs.listen(5)
 
 
-# def HTTP_parser(message):
-#     message_array = message.split("\r\n")
-#     request_line_array = message_array[0].split(" ")
-#     rest_line_array = message_array[1:-1]
-#     request_body = message_array[-1]
-#     # print(request_body)
-#     request_dict = {
-#         "Method": request_line_array[0],
-#         "URI": request_line_array[1],
-#         "Protocol": request_line_array[2],
-#     }
-#     rest_dict = {
-#         header.split(": ")[0]: header.split(": ")[1]
-#         for header in rest_line_array
-#         if header != ""
-#     }
-#     return request_dict | rest_dict | {"Body": parse_json(request_body)}
+def HTTP_non_body_parser(message):
+    message_array = message.split("\r\n")
+    request_line_array = message_array[0].split(" ")
+    rest_line_array = message_array[1:-1]
+    request_dict = {
+        "Method": request_line_array[0],
+        "URI": request_line_array[1],
+        "Protocol": request_line_array[2],
+    }
+    rest_dict = {
+        header.split(": ")[0]: header.split(": ")[1]
+        for header in rest_line_array
+        if header != ""
+    }
+    return request_dict | rest_dict
 
 
 def HTTP_parser(message):
-    message_array = message.split("\r\n\n")
-    print(message_array[0])
-    print(message_array[1])
-    return message_array[1]
+    message_array = message.split("\r\n\r\n")
+    if message_array[1] == "":
+        return HTTP_non_body_parser(message)
+    request_body = message_array[1]
+    return HTTP_non_body_parser(message) | {"Body": parse_json(request_body)}
 
 
 while True:
     clientsocket, address = hs.accept()
     message = clientsocket.recv(10000).decode("utf-8")
-    print(message)
-    HTTP_parser(message)
+    print(HTTP_parser(message))
     send_message = "Thank you for the request!"
     clientsocket.send(bytes(send_message, "utf-8"))
